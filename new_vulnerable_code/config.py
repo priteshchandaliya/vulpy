4b169a0a735c1c398649c24d93a4a9d9f344ca93
Pritesh Chandaliya <priteshchandaliya@gmail.com>
2025-05-15 16:38:21 -0700

    add vulnerable code in 2  files to pick up new issues by the scanner

--- new_vulnerable_code/config.py
+++ new_vulnerable_code/config.py
@@ -5,3 +5,5 @@ AWS_SECRET_ACCESS_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
 # Database credentials (Secret Leakage)
 DATABASE_USER = 'admin'
 DATABASE_PASSWORD = 'Password123!'
+
+# adding a comment so I can trigger harness pipeline
